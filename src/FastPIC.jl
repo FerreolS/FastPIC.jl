e02c0f1fb9fca65c2d6ef0ae100c1076060874d1
ferreol <ferreol.soulez@univ-lyon1.fr>
2025-09-11 20:24:40 +0200

    Move code from PIC to FastPIC

--- src/FastPIC.jl
+++ src/FastPIC.jl
@@ -1,5 +1,26 @@
 module FastPIC
 
-# Write your package code here.
+using ConcreteStructs,
+    DelimitedFiles,
+    EasyFITS,
+    LinearAlgebra,
+    OhMyThreads,
+    Optimisers,
+    OptimPackNextGen,
+    OptimPackNextGen.Powell.Newuoa,
+    Parameters,
+    ProgressMeter,
+    Random,
+    StaticArrays,
+    StatsBase,
+    TwoDimensional,
+    WeightedData,
+    ZippedArrays
+
+
+include("calibration.jl")
+include("profile.jl")
+include("profile_calibration.jl")
+include("spectral_calibration.jl")
 
 end
