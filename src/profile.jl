78380d06ebce51a28e62b435096074522687550c
ferreol <ferreol.soulez@univ-lyon1.fr>
2025-09-11 20:24:40 +0200

    Move code from PIC to FastPIC

---
+++ src/profile.jl
@@ -0,0 +1,106 @@
+
+struct Profile{N}
+    bbox::BoundingBox{Int64}
+    ycenter::Float64
+    cfwhm::Array{Float64,N}
+    cx::Vector{Float64}
+end
+
+Profile(bbox::BoundingBox{Int}, cfwhm::AbstractArray, cx::AbstractVector) =
+    Profile(bbox, mean(axes(bbox, 2)), cfwhm, cx)
+
+((; bbox, ycenter, cfwhm, cx)::Profile)() = get_profile(bbox, ycenter, cfwhm, cx)
+
+
+((; bbox, ycenter, cfwhm, cx)::Profile)(bbox2::BoundingBox{Int}) = get_profile(bbox2, ycenter, cfwhm, cx)
+
+function get_profile(bbox::BoundingBox{Int64}, ycenter::Float64, cfwhm::Array{Float64,N}, cx::Vector{Float64}) where N
+
+
+    xorder = length(cx)
+    fwhmorder = size(cfwhm, 1)
+
+    order = max(xorder, fwhmorder)
+
+    ax, ay = axes(bbox)
+    ypo = ((ay .- ycenter)) .^ reshape(0:order, 1, order + 1)
+
+    xcenter = ypo[:, 1:xorder] * cx
+
+    width = ypo[:, 1:fwhmorder] * cfwhm
+
+    fwhm2sigma = 1 / (2 * sqrt(2 * log(2)))
+    fw = @. -1 / (2 * (width * fwhm2sigma)^2)
+
+    xc = (ax .- xcenter')
+    if N == 1
+        xc = (ax .- xcenter')
+        dist = (xc .^ 2) .* reshape(fw, 1, :)
+    elseif N == 2
+        dist = min.(xc, 0) .^ 2 .* reshape(fw[:, 1], 1, :) .+ max.(xc, 0) .^ 2 .* reshape(fw[:, 2], 1, :)
+    else
+        error("get_profile : N must be 1 or 2")
+    end
+
+
+    img = exp.(dist)
+    return img #./ sum(img; dims=1)
+end
+
+
+function get_bbox(center_x::Float64, center_y::Float64; bbox_params::BboxParams=BboxParams())
+    @unpack_BboxParams bbox_params
+    bbox = round(
+        Int,
+        BoundingBox(; xmin=center_x - BBOX_DX_LOWER,
+            xmax=center_x + BBOX_DX_UPPER,
+            ymin=center_y - BBOX_DY_LOWER,
+            ymax=center_y + BBOX_DY_UPPER),
+        RoundNearestTiesUp) # rounding mode to preserve bbox size
+
+    size(bbox) == (BBOX_WIDTH, BBOX_HEIGHT) || return missing
+    ((bbox.xmin ≥ 1) & (bbox.xmax ≤ 2048) & (bbox.ymin ≥ 1) & (bbox.ymax ≤ 2048)) || return missing
+    return bbox
+end
+
+
+function extract_model(data::WeightedArray{T,N},
+    profile::Profile;
+    restrict=0.01,
+    nonnegative=false,
+    relative=false
+) where {T,N}
+    bbox = profile.bbox
+    if relative
+        (; value, precision) = data
+    else
+        if N > 2
+            (; value, precision) = view(data, bbox, :)
+        else
+            (; value, precision) = view(data, bbox)
+        end
+    end
+    model = profile()
+    if restrict > 0
+        model .*= (model .> restrict)
+    end
+
+    αprecision = dropdims(sum(model .^ 2 .* precision, dims=1), dims=1)
+    α = dropdims(sum(model .* precision .* value, dims=1), dims=1) ./ αprecision
+
+    nanpix = .!isnan.(α)
+    if nonnegative
+        positive = nanpix .& (α .>= T(0))
+    else
+        positive = nanpix
+    end
+
+    return WeightedArray(positive .* α, positive .* αprecision)
+end
+
+get_wavelength(coefs, ref, pixel) = get_wavelength(Val(length(coefs) - 1), Val(length(pixel)), coefs, ref, pixel)
+
+function get_wavelength(::Val{order}, ::Val{len}, coefs, ref, pixel) where {order,len}
+    fullA = SMatrix{len,order + 1}(((pixel .- ref) ./ ref) .^ reshape(0:order, 1, :))
+    return fullA * coefs
+end
---
+++ src/profile_calibration.jl
@@ -0,0 +1,138 @@
+
+
+function calibrate_profile(lamp,
+    ; calib_params::FastPICParams=FastPICParams(),
+    valid_lenslets::AbstractVector{Bool}=trues(calib_params.NLENS)
+)
+
+
+    @unpack_FastPICParams calib_params
+    @unpack_BboxParams bbox_params
+
+    size(valid_lenslets) == (NLENS,) || throw(ArgumentError("valid_lenslets must be of size NLENS"))
+
+
+
+
+    bboxes, profiles = initialize_profile!(valid_lenslets,
+        lamp;
+        calib_params=calib_params)
+
+    profile_type = ZippedVector{WeightedValue{Float64},2,true,Tuple{Vector{Float64},Vector{Float64}}}
+    lamp_spectra = Vector{profile_type}(undef, NLENS)
+
+    progress = Progress(sum(valid_lenslets); showspeed=true)
+    #Threads.@threads for i in findall(valid_lenslets)
+    # from https://discourse.julialang.org/t/optionally-multi-threaded-for-loop/81902/8?u=skleinbo
+    _foreach = multi_thread ? OhMyThreads.tforeach : Base.foreach
+    @allow_boxed_captures _foreach(findall(valid_lenslets)) do i
+        if sum(view(lamp, bboxes[i]).precision) == 0
+            valid_lenslets[i] = false
+        else
+            try
+
+                profiles[i] = fit_profile(lamp, profiles[i])
+                if any(isnan.(profiles[i].cfwhm))
+                    throw("NaN found in profile for lenslet $i")
+                end
+                lamp_spectra[i] = extract_model(lamp, profiles[i])
+
+            catch e
+                @debug "Error on lenslet $i" exception = (e, catch_backtrace())
+                valid_lenslets[i] = false
+            end
+        end
+        next!(progress)
+    end
+    ProgressMeter.finish!(progress)
+
+    model = zeros(Float64, size(lamp))
+
+    progress = Progress(sum(valid_lenslets) .* profile_loop; showspeed=true)
+
+    for _ ∈ 1:profile_loop
+        res = lamp .- model
+        model = zeros(Float64, size(lamp))
+        for i ∈ findall(valid_lenslets)
+            resi = WeightedArray(view(res, profiles[i].bbox).value .+ profiles[i]() .* reshape(lamp_spectra[i].value, 1, :), view(res, profiles[i].bbox).precision)
+            # resi = view(res,profiles[i].bbox) .+ profiles[i]() .* reshape(lamp_spectra[i].value, 1, :)
+
+            profiles[i] = fit_profile(resi, profiles[i]; relative=true)
+            if any(isnan.(profiles[i].cfwhm))
+                valid_lenslets[i] = false
+                continue
+            end
+            lamp_spectra[i] = extract_model(resi, profiles[i]; relative=true)
+            if any(isnan.(lamp_spectra[i]))
+                valid_lenslets[i] = false
+                continue
+            end
+            (; xmin, xmax, ymin, ymax) = profiles[i].bbox
+            lbox = BoundingBox(xmin=xmin - extra_width, xmax=xmax + extra_width, ymin=ymin, ymax=ymax)
+            p = profiles[i](lbox)
+            view(model, lbox) .+= p .* reshape(lamp_spectra[i].value, 1, :)
+            next!(progress)
+        end
+    end
+    ProgressMeter.finish!(progress)
+
+    return profiles, bboxes, valid_lenslets, lamp_spectra, model
+end
+
+function initialize_profile!(valid_lenslets,
+    lamp;
+    calib_params::FastPICParams=FastPICParams())
+
+    @unpack_FastPICParams calib_params
+    @unpack_BboxParams bbox_params
+
+
+    bboxes = fill(BoundingBox{Int}(nothing), NLENS)
+    profiles = Vector{Profile}(undef, NLENS)
+
+
+    @inbounds for i in findall(valid_lenslets)
+        bbox = get_bbox(lasers_cxy0s_init[i, 1], lasers_cxy0s_init[i, 2]; bbox_params=bbox_params)
+        if ismissing(bbox)
+            valid_lenslets[i] = false
+        else
+            bboxes[i] = bbox
+            profiles[i] = Profile(bbox, lamp_cfwhms_init, vcat(get_meanx(lamp, bbox), zeros(profile_order)))
+        end
+    end
+    return bboxes, profiles
+end
+
+function get_meanx(data::WeightedArray{T,N}, bbox; relative=false) where {T,N}
+    if relative
+        (; value, precision) = data
+    else
+        (; value, precision) = view(data, bbox)
+    end
+    ax, _ = axes(bbox)
+
+    return sum(value .* sqrt.(precision) .* ax) ./ sum(sqrt.(precision) .* value)
+end
+
+
+function fit_profile(data::WeightedArray{T,N},
+    profile::Profile{M};
+    relative=false) where {T,N,M}
+
+    fwhmorder = size(profile.cfwhm, 1)
+    cxorder = length(profile.cx)
+    if M == 1
+        scale = vcat(10. .^ (-(1:(fwhmorder))), 10. .^ (-(1:(cxorder))))
+    else
+        scale = vcat(10. .^ (-(1:(fwhmorder))), 10. .^ (-(1:(cxorder))), 10. .^ (-(1:(cxorder))))
+    end
+
+    vec, re = Optimisers.destructure(profile)
+
+    d = relative ? data : view(data, profile.bbox)
+    f(x) = likelihood(ScaledL2Loss(dims=1, nonnegative=true), d, re(x)())
+    Newuoa.optimize!(f, vec, 1, 1e-9; scale=scale, check=false, maxeval=10_000, verbose=0)
+    return re(vec)
+end
+
+build_loss(data, re) = x -> likelihood(ScaledL2Loss(dims=1, nonnegative=true), data, re(x)())
