63b4e117e01b1b21c4919b0affaadc71adf7f6ed
buthanoid <portail@buthanoid.eu>
2025-11-27 16:24:38 +0100

    io import. fix things in export

--- src/io.jl
+++ src/io.jl
@@ -8,11 +8,11 @@ function export_calib(fitspath, profiles, lamp_spectra, template, transmission, 
         templatehdu["EXTNAME"] = ("TEMPLATE", "Common lamp template spectrum")
         templatehdu["HDUNAME"] = ("TEMPLATE", "Common lamp template spectrum")
 
-        templatehdu["CRVAL1"] = (1.3772996728640695e-6, "Coordinate value at reference point")
+        templatehdu["CRVAL1"] = (first(lλ), "Coordinate value at reference point")
         templatehdu["CRPIX1"] = (1.0, "Pixel coordinate of reference point")
         templatehdu["CUNIT1"] = ("meter", "Units of coordinate increment and value")
         templatehdu["CTYPE1"] = ("LINEAR", "Coordinate type code")
-        templatehdu["CDELT1"] = (5.415981589438292e-9, "Coordinate increment at reference point")
+        templatehdu["CDELT1"] = (step(lλ), "Coordinate increment at reference point")
 
         write(templatehdu, template)
 
@@ -23,8 +23,9 @@ function export_calib(fitspath, profiles, lamp_spectra, template, transmission, 
 
         size_cfwhm = size(profiles[fi].cfwhm)
         size_cx = size(profiles[fi].cx)
-        size_spectral_coefs = size(profiles[fi].spectral_coefs)
-        
+        size_spectral_coefs = isnothing(profiles[fi].spectral_coefs) ? 0 :
+                              length(profiles[fi].spectral_coefs)
+
         fi = findfirst(!isnothing, transmission)
         isnothing(fi) && error("every transmission is set to `nothing`")
         length_transmission_factors = length(transmission[fi].value)
@@ -40,6 +41,10 @@ function export_calib(fitspath, profiles, lamp_spectra, template, transmission, 
         
         profilehdu["EXTNAME"] = ("PROFILES", "parametric model of each spectrum")
         profilehdu["HDUNAME"] = ("PROFILES", "parametric model of each spectrum")
+        
+        profilehdu["T"] = (string(profiles[fi].type), "numeric type for computations")
+        profilehdu["N"] = (ndims(profiles[fi].cfwhm), "ndims of coefs for FWHM variation")
+        profilehdu["SIZE_C"] = (size_spectral_coefs, "nb of coefs for wavelength solution")
 
         write(profilehdu, "BBOX" => stack([
             isnothing(p) ? [0,0,0,0] : [p.bbox[1][1], p.bbox[1][2], p.bbox[2][1], p.bbox[2][2]]
@@ -64,5 +69,54 @@ function export_calib(fitspath, profiles, lamp_spectra, template, transmission, 
             for t in transmission ]))
 
         nothing
+    end
+end
+
+function import_calib(fitspath)
+    FitsFile(fitspath) do fits
+        
+        # === TEMPLATE === #
+        template = read(fits["TEMPLATE"])
+        
+        # === lλ === #
+        start_λ = fits["TEMPLATE"]["CRVAL1"].float
+        step_λ = fits["TEMPLATE"]["CDELT1"].float
+        size_λ = length(template)
+        lλ = range(start=start_λ, step=step_λ, length=size_λ)
+        
+        T = fits["PROFILES"]["T"].string == "Float32" ? Float32 :
+            fits["PROFILES"]["T"].string == "Float64" ? Float64 :
+            error("cannot parse type")
+        N = fits["PROFILES"]["N"].integer
+        size_C = fits["PROFILES"]["SIZE_C"].integer
+        C = iszero(size_C) ? Nothing : Vector{Float64}
+        
+        nblenslets = fits["PROFILES"].data_size[1]
+        profiles = Vector{Union{Nothing,Profile{T,N,C}}}(undef, nblenslets)
+        transmission = Vector{Union{Nothing,WeightedArray{Float64,1}}}(undef, nblenslets)
+        P = read(fits["PROFILES"])
+        for i in 1:nblenslets
+            # === PROFILES === #
+            if all(iszero, P["BBOX"][:,i])
+                profiles[i] = nothing
+            else
+                bbox = BoundingBox{Int}(Point(P["BBOX"][1,i], P["BBOX"][2,i]),
+                                          Point(P["BBOX"][3,i], P["BBOX"][4,i]))
+                ycenter = P["YCENTER"][i]
+                cfwhm = P["CFWHM"][ fill(Colon(), N)..., i]
+                cx = P["CX"][:,i]
+                spectral_coefs = iszero(size_C) ? Nothing : P["SPECTRAL_COEFS"][:,i]
+                profiles[i] = Profile(T, bbox, ycenter, cfwhm, cx, spectral_coefs)
+            end
+            # === TRANSMISSION === #
+            if all(isnan, P["TRANSMISSION"][:,:,i])
+                transmission[i] = nothing
+            else
+                transmission[i] = WeightedArray(P["TRANSMISSION"][:,1,i],
+                                                P["TRANSMISSION"][:,2,i])
+            end
+        end
+
+        (profiles, template, transmission, lλ)
     end
 end
